407f6c7970a90d19fd356a3fbb056424f753b1a2
Zach <thompzac@oregonstate.edu>
2019-05-09 19:10:32 -0700

    Adding base code to repository

---
+++ TestGameServer/TestGameServer/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TestGameServer
+{
+    class Client
+    {
+        private Socket c_sock; //Client network socket
+        private int c_id;   //Client network id
+
+
+    }
+}
---
+++ TestGameServer/TestGameServer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace TestGameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Socket s = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Any, 65500);
+            s.Bind(ipe);
+            s.Listen(10);
+            Console.WriteLine("Listening");
+
+            while (true)
+            {
+                Socket client = s.Accept();
+
+
+                /*
+                 * Spawn test:
+                 * send 3, 4 byte integers in order: X, Y, Z to spawn an object
+                 * 
+                 */
+
+                byte[] x = BitConverter.GetBytes((Int32)(-7470));
+                byte[] y = BitConverter.GetBytes((Int32)(2980));
+                byte[] z = BitConverter.GetBytes((Int32)(200));
+                byte[] buff = new byte[4 * 3];
+                x.CopyTo(buff, 0);
+                y.CopyTo(buff, 4);
+                z.CopyTo(buff, 8);
+                client.Send(buff);
+                StringBuilder hex = new StringBuilder(4 * 3 * 2);
+                foreach (byte b in buff)
+                {
+                    hex.AppendFormat("{0:x2} ", b);
+                }
+                Console.WriteLine(hex.ToString());
+                Console.WriteLine("Socket accepted!");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
